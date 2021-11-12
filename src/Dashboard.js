3836c3e696b8973bb15632712d50c04c6cc59457
gabriel-5 <82604791+gabriel-5@users.noreply.github.com>
2021-11-12 10:13:46 +0000

    Create tab interface

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -3,8 +3,10 @@ import React, { useState, useEffect } from "react";
 import Add from "./Add";
 import Button from "react-bootstrap/Button";
 import moment from "moment";
-import { Table, Thead, Tbody, Tr, Th, Td } from 'react-super-responsive-table';
-import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css';
+import { Table, Thead, Tbody, Tr, Th, Td } from "react-super-responsive-table";
+import "react-super-responsive-table/dist/SuperResponsiveTableStyle.css";
+import Tabs from "react-bootstrap/Tabs";
+import Tab from "react-bootstrap/Tab";
 
 function Dashboard(props) {
   const [quotes, cQuotes] = useState([]);
@@ -34,20 +36,21 @@ function Dashboard(props) {
           <td>{current.areas}</td>
           <td>{current.jobDescription}</td>
           <td>{current.productsRequired}</td>
-          <td>{current.cost}</td>
+          <td>£{current.cost}</td>
 
           {/* moment().format('MMMM Do YYYY, h:mm:ss a') */}
           <td>{moment(current.date).format("MMMM Do YYYY, h:mm a")}</td>
           <td>
-            <Button 
-            className="mx-1" 
-            onClick={() => removeQuote(current._id)} 
-            variant="danger">
+            <Button
+              className="mx-1"
+              onClick={() => removeQuote(current._id)}
+              variant="danger"
+            >
               Remove
             </Button>
 
             <Button
-            className="mx-1"
+              className="mx-1"
               onClick={() => updateQuote(current)}
               type="button"
               variant="warning"
@@ -63,21 +66,32 @@ function Dashboard(props) {
   return (
     <>
       <br />
-      <Table className="mx-4" size="sm" responsive="md" fluid>
-        <thead>
-          <tr>
-            <th>Rooms</th>
-            <th>Areas</th>
-            <th>Job Details</th>
-            <th>Products</th>
-            <th>Cost</th>
-            <th>Date</th>
-          </tr>
-        </thead>
-        <tbody>{buildrows()}</tbody>
-      </Table>
-      <br />
-      <br />
+
+      <Tabs
+        defaultActiveKey="profile"
+        id="uncontrolled-tab-example"
+        className="mb-3"
+      >
+        <Tab eventKey="quotes" title="Quotes">
+          <Table className="mx-4" size="sm" responsive="md" fluid>
+            <thead>
+              <tr>
+                <th>Rooms</th>
+                <th>Areas</th>
+                <th>Job Details</th>
+                <th>Products</th>
+                <th>Cost</th>
+                <th>Date</th>
+              </tr>
+            </thead>
+            <tbody>{buildrows()}</tbody>
+          </Table>
+          <br />
+          <br />
+        </Tab>
+        <Tab eventKey="inProgress" title="Jobs In Progress"></Tab>
+        <Tab eventKey="completed" title="Completed Jobs"></Tab>
+      </Tabs>
       <Add
         client={props.client}
         refreshList={() => {
